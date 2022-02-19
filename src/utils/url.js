d7d2d6487b441eec6888a191881620d4a3d18b9f
Navdeep <omkumar@Oms-MacBook-Air.local>
2022-02-19 16:45:20 +0530

    New changes

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,9 @@
-export const BACKEND_URL = "http://13.235.31.216/api"
-export const IMAGE_URL = "http://13.235.31.216/uploads"
+export const BACKEND_URL = "http://13.235.31.216/api";
+export const IMAGE_URL = "http://13.235.31.216/uploads";
+
+// git config --global user.name "Your Name"
+//     git config --global user.email you@example.com
+
+// After doing this, you may fix the identity used for this commit with:
+
+//     git commit --amend --reset-author
